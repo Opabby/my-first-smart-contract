caa105038738c9f1020232c798e317ef4b35dde7
Thaís Rodeiro <ttheinterpreter@gmail.com>
2022-04-25 17:01:26 -0300

    Working on the wave-portal

--- scripts/run.js
+++ scripts/run.js
@@ -1,37 +1,33 @@
 const main = async () => {
     const [owner, randomPerson] = await hre.ethers.getSigners();
+  
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
-
-    console.log("Contract deployed to:", waveContract.address);
-    console.log("Contract deployed by:", owner.address);
-
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves();
-
-    let waveTxn = await waveContract.wave();
-    await waveTxn.wait();
-    
-    waveCount = await waveContract.getTotalWaves();
-
-    waveTxn = await waveContract.connect(randomPerson).wave();
-    await waveTxn.wait();
-
-    waveCount = await waveContract.getTotalWaves();
-};
-
-const runMain = async () => {
+    console.log("Contract addy:", waveContract.address);
+  
+    let waveCount = await waveContract.getTotalWaves();
+    console.log(waveCount.toNumber()); // this should print out 0
+  
+    let waveTxn = await waveContract.wave("A message!");
+    await waveTxn.wait(); 
+  
+   // const [_, randomPerson] = await hre.ethers.getSigners();
+    waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+    await waveTxn.wait(); 
+  
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
+  };
+  
+  const runMain = async () => {
     try {
-        await main();
-        process.exit(0);
-        // exit node process without error
-    } catch(error) {
-        console.log(error);
-        process.exit(1);
-        // exit node process while indicating 'Uncaught Fatal Exception' error
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
     }
-    // read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
-}
-
-runMain();
+  };
+  
+  runMain();
